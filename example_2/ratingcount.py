8f37d985317b0d5e1dc626a8aecf57621a7810bc
MaciejKowalczyk98 <61247254+MaciejKowalczyk98@users.noreply.github.com>
2020-10-26 22:52:11 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -7,7 +7,7 @@ class MRHotelRaitingCount(MRJob):
         yield result
         
     def reducer(self, key, value):
-        result = [key, sum(value)]
+        result = [key, value]
         yield result
         
 if __name__ == '__main__':
