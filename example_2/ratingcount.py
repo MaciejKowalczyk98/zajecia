d623ee74e6d39c22301f8e0196bf0221f1745727
MaciejKowalczyk98 <61247254+MaciejKowalczyk98@users.noreply.github.com>
2020-10-26 23:31:57 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -5,11 +5,11 @@ class MRHotelRaitingCount(MRJob):
     def mapper(self, _, line):
         (HName, HStar, HRooms, UCountry, NrReviews, rating, StayPeriod, TType, Pool, Gym, TCourt, Spa, Casino, Internet,
          UContinent, ReviewMonth, ReviewDay) = line.split("\t")
-        result = [HName, rating]
+        result = [HName, 1]
         yield result
 
     def reducer(self, key, value):
-        result = (key, value)  
+        result = (key, (len)value)  
         yield result 
 
 
