19185a04d5df5865e864b66de527c1f224bcbc46
MaciejKowalczyk98 <61247254+MaciejKowalczyk98@users.noreply.github.com>
2020-10-26 22:58:12 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -3,7 +3,7 @@ from mrjob.job import MRJob
 class MRHotelRaitingCount(MRJob):
     def mapper(self, _, line):
         (HName, HStar, HRooms, UCountry, NrReviews, rating, StayPeriod, TType, Pool, Gym, TCourt, Spa, Casino, Internet, UContinent, ReviewMonth, ReviewDay) = line.split("\t")
-        result = [HName/rating]
+        result = [HName, rating]
         yield result
  
 if __name__ == '__main__':
