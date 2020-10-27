8647417ec159bc78f55aa80e1262d7e9dce5ec36
MaciejKowalczyk98 <61247254+MaciejKowalczyk98@users.noreply.github.com>
2020-10-27 16:08:28 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -9,23 +9,15 @@ class MRHotelRaitingCount(MRJob):
         x = rating
         x = float(x)
         
-        wynik = [HName, x]
-        
-       
-        result = [HName, 1]
+        wynik = [HName, x] // suma ocen poszczególnych hoteli    
+        result = [HName, 1] // ilość poszczególnych hoteli 
         
         yield wynik
-        yield result    
-        
-        
-
- 
+        yield result           
         
     def reducer(self, key, value):
         result = [key, sum(value)]
         yield result
-
-
 
 if __name__ == '__main__':
     MRHotelRaitingCount.run()
