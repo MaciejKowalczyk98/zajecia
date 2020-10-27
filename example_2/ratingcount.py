c8c27db41d21f2b8441c610c0c1c5edaa3491055
MaciejKowalczyk98 <61247254+MaciejKowalczyk98@users.noreply.github.com>
2020-10-27 18:55:32 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -3,9 +3,8 @@ from mrjob.job import MRJob
 
 class MRHotelRaitingCount(MRJob):
     def mapper(self, _, line):
-        h = (x, z, y, k) 
-        h = line.split(",")
-        result = [k, 1]
+        (a, s, d, f, g, h, j, k)  = line.split(",")
+        result = [s, 1]
 
         yield result
    
