07e9922bf302b791603f55f710d4edad1819b216
MaciejKowalczyk98 <61247254+MaciejKowalczyk98@users.noreply.github.com>
2020-10-27 16:09:16 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -9,8 +9,8 @@ class MRHotelRaitingCount(MRJob):
         x = rating
         x = float(x)
         
-        wynik = [HName, x] // suma ocen poszczególnych hoteli    
-        result = [HName, 1] // ilość poszczególnych hoteli 
+        wynik = [HName, x]    #suma ocen poszczególnych hoteli    
+        result = [HName, 1]   #ilość poszczególnych hoteli 
         
         yield wynik
         yield result           
