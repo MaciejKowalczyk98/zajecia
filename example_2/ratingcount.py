81d2ec8f5510c81ab7bbc805ba09476645e7adfc
MaciejKowalczyk98 <61247254+MaciejKowalczyk98@users.noreply.github.com>
2020-10-27 19:08:00 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -3,8 +3,14 @@ from mrjob.job import MRJob
 
 class MRHotelRaitingCount(MRJob):
     def mapper(self, _, line):
-        (a, s, d, f)  = line.replace(' ','').replace('N/A','').split(',')
+        (a, s, d, f)  = line.split(",")
+        csv_reader = csv.reader(line)
+        next(csv_reader)
         result = [s, 1]
+        
+        file = open('sample.csv')
+        
+
 
         yield result
    
