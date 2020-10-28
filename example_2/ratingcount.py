555c0a5f58c8f7f8df7e60804fac573285526a1c
MaciejKowalczyk98 <61247254+MaciejKowalczyk98@users.noreply.github.com>
2020-10-28 12:24:55 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -1,8 +1,37 @@
 from mrjob.job import MRJob
+import os
 
 class MRHotelRaitingCount(MRJob):
-    file_name = os.environ['mapreduce_map_input_file']
-    print  (file_name)
+    def mapper(self, key, line):
+        splits = []
+        idKey = 0
+        if "ratings" in os.environ['map_input_file']:
+            splits = line.split(',')
+            idKey = 1
+        else:
+            splits = line.split(',')
+            idKey = 0
+        yield splits[idKey],splits
+
+    def reducer(self, movieID, value):
+        a = []
+        for x in value:
+            a.append(x)
+
+        if len(a) == 1:
+            yield movieID, 'No Rating'
+        else:
+            total = 0
+            elements = 0
+            if len (a[-1]) == 3:
+                title = a[-1][1]
+            else:
+                title = ''.join(a[-1][1:-1])
+            for x in a[:-1]:
+                total +=float(x[2])
+                elements += 1
+
+            yield movieID, {"title": title, "rating": total / elements}
 
 if __name__ == '__main__':
     MRHotelRaitingCount.run()
